7a2f1111ef3fcd61117d0c88dd998305f78ea963
Anthony <tejant96@gmail.com>
2020-10-31 18:44:51 -0400

    Revert "Manger Repo Interface"
    This reverts commit 9d3a57fb450ee9d9f746791e35a78706e6d8e632.

--- P0/StoreApp/StoreAppDB/Interfaces/IManagerRepoActions.cs
+++ P0/StoreApp/StoreAppDB/Interfaces/IManagerRepoActions.cs
@@ -1,11 +1,9 @@
 using StoreAppDB.Models;
-using System.Collections.Generic;
 
 namespace StoreAppDB.Interfaces
 {
     public interface IManagerRepoActions
     {
-        void AddBaseballBatIntoTable(BaseballBat baseballBat);
-        List<BaseballBat> GetAllBaseballBats();
+        void AddEmployeeAsync(Manager manager);
     }
 }
